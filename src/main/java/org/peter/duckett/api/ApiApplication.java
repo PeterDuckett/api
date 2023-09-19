2697cbb01b6f17ef91c6993327357a6ad6f00996
Peter James Duckett <peter.james.duckett@googlemail.com>
2023-09-19 16:05:16 +0100

    Initial commit2

--- src/main/java/org/peter/duckett/api/ApiApplication.java
+++ src/main/java/org/peter/duckett/api/ApiApplication.java
@@ -2,10 +2,16 @@ package org.peter.duckett.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class ApiApplication {
 
+
+    @Bean
+    EmployeeRepository employeeRepository() {
+        return new EmployeeRepository();
+    }
     public static void main(String[] args) {
         SpringApplication.run(ApiApplication.class, args);
     }
