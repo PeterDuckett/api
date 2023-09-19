66a464c20e1cb11ae20578fe2586191607c2ea6a
Peter Duckett <peter.james.duckett@googlemail.com>
2023-09-19 18:46:30 +0100

    test

--- src/main/java/org/peter/duckett/api/ApiApplication.java
+++ src/main/java/org/peter/duckett/api/ApiApplication.java
@@ -7,11 +7,6 @@ import org.springframework.context.annotation.Bean;
 @SpringBootApplication
 public class ApiApplication {
 
-
-    @Bean
-    EmployeeRepository employeeRepository() {
-        return new EmployeeRepository();
-    }
     public static void main(String[] args) {
         SpringApplication.run(ApiApplication.class, args);
     }
